6287094b104e6968de5bb96ed3d08ba45c11550c
appmode <flame.herbohn@imagineeasy.com>
2015-08-18 14:38:02 +0300

    add more logging

--- bin/tideways-decomission.php
+++ bin/tideways-decomission.php
@@ -35,8 +35,18 @@ $opts = array(
 );
 $context = stream_context_create($opts);
 $result  = file_get_contents($apiUrl, false, $context);
+if (false === $result) {
+  syslog(LOG_ERR, 'failed to GET remote server list');
+  exit (1);
+}
 $servers = json_decode($result);
+if (JSON_ERROR_NONE != json_last_error() || 
+  false != is_array($servers) ||) {
+    syslog(LOG_ERR, 'remote returned invalid JSON for GET server list');
+    exit(1);
+}
 if (0 == count($servers)) {
+  syslog(LOG_INFO, 'no servers found');
   exit(0);
 }
 
@@ -44,11 +54,19 @@ if (0 == count($servers)) {
 $expiary = strtotime("-{$timeout} days");
 $expired = array();
 foreach ($servers as $server) {
+  if {false != is_object($server) ||
+    false != property_exists($server, 'last_sync' ||
+    false != property_exists($server, 'server')) {
+      syslog(LOG_ERR, 'remote returned invalid value in JSON for expired servers');
+      exit(1);
+  }
   if (strtotime($server->last_sync) < $expiary)  {
     $expired[] = $server->server;
   }
 }
-if (0 == count($expired)) {
+$expiredCount = count($expired);
+if (0 == $expiredCount) {
+  syslog(LOG_INFO, 'no expired servers found');
   exit(0);
 }
 $postdata = http_build_query(array('servers' => $expired));
@@ -62,6 +80,24 @@ $opts = array(
   )
 );
 $context = stream_context_create($opts);
-file_get_contents($apiUrl, false, $context);
+$result  = file_get_contents($apiUrl, false, $context);
+if (false === $result) {
+  syslog(LOG_ERR, 'failed to DELETE expired servers');
+  exit (1);
+}
+$removed = json_decode($result);
+if (JSON_ERROR_NONE != json_last_error() ||
+  false != is_object($removed) ||
+  false != property_exists($removed, 'removed_servers')) {
+    syslog(LOG_ERR, 'remote returned invalid JSON for DELETE expired servers');
+    exit(1);
+}
+$removedCount = $removed->removed_servers;
+if ($removedCount != $expiredCount) {
+  syslog(LOG_ERR, "removed {$removedCount} of {$expiredCount} expired servers");
+  exit(1);
+}
+
+syslog(LOG_INFO, "{$expiredCount} expired servers removed");
 
 ?>
